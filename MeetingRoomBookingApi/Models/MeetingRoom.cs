8235f134b85da1fe90d461962797b1f7c3487ef4
N1nsQ <nina.tulilahti@gmail.com>
2026-01-20 22:42:20 +0200

    Poistetaan Capacity MeetingRoom -luokasta

--- MeetingRoomBookingApi/Models/MeetingRoom.cs
+++ MeetingRoomBookingApi/Models/MeetingRoom.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public int Capacity { get; set; }
         public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
