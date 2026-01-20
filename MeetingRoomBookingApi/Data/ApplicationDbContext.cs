8f10b4fb8c9f61ea5cce5e7a5f03ebb7ce4920fc
N1nsQ <nina.tulilahti@gmail.com>
2026-01-20 21:58:12 +0200

    Poistettu huonelta Capacity --> Ei vaikuta mihinkään

--- MeetingRoomBookingApi/Data/ApplicationDbContext.cs
+++ MeetingRoomBookingApi/Data/ApplicationDbContext.cs
@@ -19,9 +19,9 @@ namespace MeetingRoomBookingApi.Data
 
             // Seed-data: Lisää muutama kokoushuone
             modelBuilder.Entity<MeetingRoom>().HasData(
-                new MeetingRoom { Id = 1, Name = "Sali A", Capacity = 10 },
-                new MeetingRoom { Id = 2, Name = "Sali B", Capacity = 6 },
-                new MeetingRoom { Id = 3, Name = "Sali C", Capacity = 4 }
+                new MeetingRoom { Id = 1, Name = "Sali A" },
+                new MeetingRoom { Id = 2, Name = "Sali B" },
+                new MeetingRoom { Id = 3, Name = "Sali C" }
             );
         }
     }
